7347753966d85947c3f7cdcffd1a64340bdf2f8e
MaksAnikeev <anikeev.maks@rambler.ru>
2022-12-27 15:14:00 +0300

    Rename related_name in OrderItems

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -244,7 +244,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
-        related_name='order_products',
+        related_name='order_items',
         verbose_name='продукт',
     )
 
